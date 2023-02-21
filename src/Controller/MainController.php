a25cba133e720e5ba290924c0feb44f9a663f63d
mlemat2022 <maxoulemat35@gmail.com>
2023-02-21 12:24:23 +0100

    Fin du tp

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,14 @@ class MainController extends AbstractController
     #[Route('/home', name: 'main_home')]
     public function home(): Response
     {
-        return $this->render("main/home.html.twig");
+
+        $username = 'Sylvain';
+        $serie = ['title' => 'Community', 'year' => 'Ouf', 'platform' => 'NBC'];
+
+        return $this->render("main/home.html.twig", [
+            "name" => $username,
+            "serie" => $serie
+        ]);
     }
 
     /**

a75cb4eeb30c114f85e0d57ff66601b5bd224512
mlemat2022 <maxoulemat35@gmail.com>
2023-02-21 10:14:13 +0100

    ajout des extends

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,11 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    #[Route('/home', name: 'main_home')]
+    public function home(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render("main/home.html.twig");
+    }
+
+    /**
+     * @Route("/test", name="main_test")
+     */
+
+    public function test(): Response
+    {
+        return $this->render("main/test.html.twig");
     }
 }
